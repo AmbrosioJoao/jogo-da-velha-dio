4eb6ac7fef5aa089ca85676b9f8ffbe3d13fc4f3
AmbrosioJoao <ambrosiojoaomateus@gmail.com>
2021-10-06 23:02:34 -0300

    first commit

---
+++ projects/script.js
@@ -0,0 +1,14 @@
+var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+
+function escolherquadrado(id){
+    var quadrado = document.getElementById()
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '0000'; 
+}
+
+function mudarjogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
+}
